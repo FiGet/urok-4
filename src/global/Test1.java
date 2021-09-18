c99dc62abc89172ff2fc2f6c0749d1b5dd6ba818
FiGet <figet91@gmail.com>
2021-09-18 21:18:11 +0300

    урок 4

---
+++ src/global/Test1.java
@@ -0,0 +1,45 @@
+package global;
+import java.util.Scanner;
+public class Test1 {
+    public static void main(String[] args) {
+        //задание 9
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ввведите число: ");
+        int number = scanner.nextInt();
+        if ( number % 2 == 0 )
+            System.out.println("Число четное");
+        else
+            System.out.println("Число нечетное");
+
+        //задание 10
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.print("Ввведите номер дня недели: ");
+        int den = scanner1.nextInt();
+        switch (den) {
+            case 1:
+                System.out.print("День недели - понедельник");
+                break;
+            case 2:
+                System.out.print("День недели - вторник");
+                break;
+            case 3:
+                System.out.print("День недели - среда");
+                break;
+            case 4:
+                System.out.print("День недели - четверг");
+                break;
+            case 5:
+                System.out.print("День недели - пятница");
+                break;
+            case 6:
+                System.out.print("День недели - суббота");
+                break;
+            case 7:
+                System.out.print("День недели - воскресенье");
+                break;
+            default:
+                System.out.print("Некорректно введен номер дня недели");
+        }
+
+    }
+}
